a4e7c07ca8d904e06de90911d18b8506a7519976
mauricecyril <25261554+mauricecyril@users.noreply.github.com>
2018-07-13 00:19:58 -0400

    Update install_bme680.sh

--- install_bme680.sh
+++ install_bme680.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
 curl https://get.pimoroni.com/i2c | bash
-cd ~
-
-
-git clone https://github.com/pimoroni/bme680
-cd bme680/library
-
-sudo python3 setup.py install
+curl https://get.pimoroni.com/bme680 | bash
